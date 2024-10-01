a601e26364e59fcf242d47adf4fa8252ecdc4c43
gorbatniova <g.gorbatniova@gmail.com>
2024-10-01 10:27:53 +0300

    20241001_mokiniu_statistika

---
+++ Užduotys/20241001_mokiniu_statistika.py
@@ -0,0 +1,13 @@
+﻿def berniukas(mokinys):
+    return True
+
+mokiniai = ['Degutis Edvardas', 'StankevičiuscEgidijus', 'Poskevičius Algirdas', 
+'Žardeckas Nedas', 'Lukoševičiūtė Vygantė', ' Navickienė Skaiva', 'Sokolnikienė Irma']
+mergaites = []
+
+for mokinys in mokiniai:
+    if berniukas(mokinys):
+        mergaites.append(mokinys)
+        
+    
+print(mergaites) 
