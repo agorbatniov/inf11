3f541a97371212e209dc4e48fa75d69563d8cc05
Andrej <54258807+agorbatniov@users.noreply.github.com>
2024-10-03 06:39:31 +0300

    Algoritmų parinkimas. Rekursija (prieš pamoką)

---
+++ Algoritmų parinkimas. Rekursija/Python/02_Fibonacci.py
@@ -0,0 +1,9 @@
+# The Fibonacci Sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+#                         1  2  3  4  5  6  7   8   9   10  11  12
+
+def fibonacci(n):
+
+        return 0
+
+
+print()
