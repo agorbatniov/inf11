b77fa7efff81202c9aca47d7c230f594f2bc5c27
Andrej <54258807+agorbatniov@users.noreply.github.com>
2024-12-12 08:28:35 +0200

    Assert test update

--- Programų testavimas ir taisymas/assert_testai.py
+++ Programų testavimas ir taisymas/assert_testai.py
@@ -4,14 +4,10 @@ def sub(a, b):
 def sum(a, b):
     return (a + b)
 
-
 try:
     assert sum(2, 3) == 5, f"Klaida vykdant funkciją 'sum()'. Atsakymas turi būti {5}" 
     assert sub(17, 10) == 7, f"Klaida vykdant funkciją 'sub()'. Atsakymas turi būti {7}" 
     print(f'Visi testai yra sėkmingi')
 except AssertionError as klaida:
-    sekmingi = False
     print(f"Testas: {klaida}")
-
-
 
