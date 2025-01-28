20678f26b65ebc8feb759a73d3c69973c6500bbf
Andrej <a.gorbatniov@gmail.com>
2025-01-28 08:58:27 +0200

    Informacijos (rezultatų) pateikimas

---
+++ Duomenų tyrybos, programavimo ir saugaus elgesio pradmenys/01-Informacijos_(rezultatų)_pateikimas/filtravimas_template.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Sukuriamas duomenų rinkinys
+data = {
+    'Vardas': ['Jonas', 'Asta', 'Lukas', 'Rūta'],
+    'Amžius': [25, 22, 24, 23],
+    'Pajamos': [500, 600, 700, 550]
+}
+
+
+# Rūšiavimas pagal pajamas
+
+
+# Filtravimas pagal amžių (>23)
+
+
+# Grupavimas (pvz., amžiaus grupės)
